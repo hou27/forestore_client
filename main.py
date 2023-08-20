89e60dc5e465ad9f25a55207b8a9582c77a05d81
hou27 <ataj125@gmail.com>
2023-08-21 02:35:04 +0900

    실제 예측값(정적)을 사용하여 시각화 구현

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import pandas as pd
 import numpy as np
 import datetime
 
-chart_data = pd.DataFrame(np.random.randn(100, 3), columns=["a", "b", "c"])
+from mocking_data import get_mocking_data
+
+chart_data = get_mocking_data()
 
 # Add a timestamp column to the data
 start_time = datetime.datetime.now()
-timestamps = pd.date_range(start=start_time, periods=100, freq="5min")
+timestamps = pd.date_range(start=start_time, periods=len(chart_data), freq="60min")
 chart_data["timestamp"] = timestamps
 
 
@@ -15,20 +17,27 @@ def main():
     st.title("명지 마트 재고 현황")
     st.write("301개 품목에 대한 재고 현황을 보여줍니다.")
 
-    # Set the timestamp column as the index
+    # timestamp column을 index로 설정
     chart_data_with_index = chart_data.set_index("timestamp")
 
-    multiselect = st.multiselect("Choose Item", ("A", "B", "C"))
+    multiselect = st.multiselect("Choose Item", (i for i in range(1, 302)))
     how_long = st.number_input(
         "How long?", min_value=10, max_value=100, value=20, step=1
     )
 
-    filtered_data = chart_data_with_index[list(map(lambda x: x.lower(), multiselect))]
+    filtered_data = chart_data_with_index[list(map(lambda x: str(x), multiselect))]
 
     if multiselect:
         line_chart = st.line_chart(filtered_data[:how_long])
     else:
         line_chart = st.line_chart(chart_data_with_index[:how_long])
+
+    # 마지막 timestamp와 재고량 출력
+    st.write("마지막 시간과 재고량")
+    # last_timestamp = chart_data_with_index.index[-1]
+    # last_values = chart_data_with_index.loc[last_timestamp].to_dict()
+    # for col, value in last_values.items():
+    #     st.write(f"{col}: {value} @ {last_timestamp}")
 
     st.dataframe(chart_data_with_index)
 
---
+++ mocking_data.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import datetime
+
+
+def get_mocking_data():
+    predictions_df = pd.read_excel("small_test_prediction_list.xlsx")
+    predictions = [
+        predictions_df.iloc[i].tolist() for i in range(1, len(predictions_df))
+    ]
+    item_ids = [i for i in range(1, 302)]
+    timestamp_base = datetime.datetime.now()
+
+    # Create timestamp list
+    timestamps = [
+        timestamp_base + datetime.timedelta(minutes=i)
+        for i in range(len(predictions[0]))
+    ]
+
+    concatenated_data = {
+        f"{item_id}": values for item_id, values in zip(item_ids, predictions)
+    }
+
+    # Create a dataframe with timestamps as the index
+    df = pd.DataFrame(concatenated_data, index=timestamps)
+    return df
