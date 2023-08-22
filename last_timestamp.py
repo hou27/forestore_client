2c0b8888d575552f98c7484a7ccbc51990791d20
hou27 <ataj125@gmail.com>
2023-08-22 17:19:21 +0900

    재고 감소가 멈췄다고 감지하는 기준 재설정

--- last_timestamp.py
+++ last_timestamp.py
@@ -4,7 +4,7 @@ def get_last_timestamp(df):
     for i in range(len(df.columns)):
         for j in range(1, len(df)):
             if (
-                df.iloc[j, i] > df.iloc[j - 1, i] + 30
+                df.iloc[j, i] > df.iloc[j - 1, i] + 8
                 or df.iloc[j, i] == 0
                 or j == len(df) - 1
             ):
