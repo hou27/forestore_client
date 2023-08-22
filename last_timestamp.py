4dd8fa8c9b7fea0cb6ef82c109bbce6e9ebd4d7f
hou27 <ataj125@gmail.com>
2023-08-22 19:56:23 +0900

    사이클 탐지 기준 강화

--- last_timestamp.py
+++ last_timestamp.py
@@ -4,9 +4,10 @@ def get_last_timestamp(df):
     for i in range(len(df.columns)):
         for j in range(1, len(df)):
             if (
-                df.iloc[j, i] > df.iloc[j - 1, i] + 8
+                (df.iloc[j, i] > df.iloc[j - 1, i] + 5 and df.iloc[j - 1, i] <= 10)
                 or df.iloc[j, i] == 0
                 or j == len(df) - 1
+                or df.iloc[j, i] > df.iloc[j - 1, i] + 20
             ):
                 length_of_cycle.append(j)
                 break
